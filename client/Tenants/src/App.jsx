aa0bd41096b1dac538bcb5bbcd2b4aa23dbc77b3
Zain Ul Abideen <zainulabideen.shah1214@gmail.com>
2024-04-26 10:37:50 +0500

    progress

--- client/Tenants/src/App.jsx
+++ client/Tenants/src/App.jsx
@@ -9,6 +9,7 @@ import PrivateRoute from './components/PrivateRoute';
 import CreateListing from './pages/CreateListing';
 import UpdateListing from './pages/UpdateListing';
 import Listing from './pages/Listing';
+import Search from './pages/Search';
 
 function App() {
 
@@ -20,6 +21,7 @@ function App() {
         <Route path='/signin' element={<Signin />} />
         <Route path='/signup' element={<Signup />} />
         <Route path='/about' element={<About />} />
+        <Route path='/search' element={<Search />} />
         <Route path='/listing/:listingId' element={<Listing />} />
         <Route element={<PrivateRoute />} >
           <Route path='/profile' element={<Profile />} />
